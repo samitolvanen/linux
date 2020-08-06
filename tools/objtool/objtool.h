835567cbe55dd48e1fd86c06f06efdfca423baad
Peter Zijlstra <peterz@infradead.org>
2020-08-06 15:14:09 -0700

    objtool: Add a pass for generating __mcount_loc
    Add the --mcount option for generating __mcount_loc sections
    needed for dynamic ftrace. Using this pass requires the kernel to
    be compiled with -mfentry and CC_USING_NOP_MCOUNT to be defined
    in Makefile.
    
    Link: https://lore.kernel.org/lkml/20200625200235.GQ4781@hirez.programming.kicks-ass.net/
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    [Sami: rebased to mainline, dropped config changes, fixed to actually use
           --mcount, and wrote a commit message.]
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>

--- tools/objtool/objtool.h
+++ tools/objtool/objtool.h
@@ -16,6 +16,7 @@ struct objtool_file {
 	struct elf *elf;
 	struct list_head insn_list;
 	DECLARE_HASHTABLE(insn_hash, 20);
+	struct list_head mcount_loc_list;
 	bool ignore_unreachables, c_file, hints, rodata;
 };
 
