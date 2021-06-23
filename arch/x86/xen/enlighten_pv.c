52cbf2a506edd2c4f42ff26cede807502da5a261
Sami Tolvanen <samitolvanen@google.com>
2021-06-23 09:48:58 -0700

    x86: Use an opaque type for functions not callable from C
    The kernel has several assembly functions that are not directly callable
    from C. Use an opaque type for these function prototypes to make misuse
    harder, and to avoid the need to annotate references to these functions
    for Clang's Control-Flow Integrity (CFI).
    
    Suggested-by: Andy Lutomirski <luto@amacapital.net>
    Suggested-by: Alexander Lobakin <alobakin@pm.me>
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/x86/xen/enlighten_pv.c
+++ arch/x86/xen/enlighten_pv.c
@@ -613,8 +613,8 @@ DEFINE_IDTENTRY_RAW(xenpv_exc_machine_check)
 #endif
 
 struct trap_array_entry {
-	void (*orig)(void);
-	void (*xen)(void);
+	asm_func_t orig;
+	asm_func_t xen;
 	bool ist_okay;
 };
 
@@ -673,7 +673,7 @@ static bool __ref get_trap_addr(void **addr, unsigned int ist)
 		struct trap_array_entry *entry = trap_array + nr;
 
 		if (*addr == entry->orig) {
-			*addr = entry->xen;
+			*addr = (void *)entry->xen;
 			ist_okay = entry->ist_okay;
 			found = true;
 			break;
