dcce350b94263083709980e3f2726059ecf9ed9c
Sami Tolvanen <samitolvanen@google.com>
2022-05-09 22:57:12 +0000

    arm64: Drop unneeded __nocfi attributes
    With -fsanitize=kcfi, CONFIG_CFI_CLANG no longer has issues
    with address space confusion in functions that switch to linear
    mapping. Now that the indirectly called assembly functions have
    type annotations, drop the __nocfi attributes.
    
    Suggested-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>

--- arch/arm64/include/asm/mmu_context.h
+++ arch/arm64/include/asm/mmu_context.h
@@ -143,7 +143,7 @@ static inline void cpu_install_ttbr0(phys_addr_t ttbr0, unsigned long t0sz)
  * Atomically replaces the active TTBR1_EL1 PGD with a new VA-compatible PGD,
  * avoiding the possibility of conflicting TLB entries being allocated.
  */
-static inline void __nocfi cpu_replace_ttbr1(pgd_t *pgdp)
+static inline void cpu_replace_ttbr1(pgd_t *pgdp)
 {
 	typedef void (ttbr_replace_func)(phys_addr_t);
 	extern ttbr_replace_func idmap_cpu_replace_ttbr1;
