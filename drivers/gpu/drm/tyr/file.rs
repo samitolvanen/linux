dece3e3733124227b71cf846605c0c6fb25e884f
Sami Tolvanen <samitolvanen@google.com>
2026-06-19 21:36:31 -0700

    drm/tyr: file: report the GPU timestamp frequency
    DEV_QUERY_TIMESTAMP_INFO returned a zero timestamp_frequency, so userspace
    could not convert GPU timestamps to time. Report the arch timer rate, as
    Panthor does (zero only when the arch timer is absent).
    
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- drivers/gpu/drm/tyr/file.rs
+++ drivers/gpu/drm/tyr/file.rs
@@ -26,6 +26,7 @@ use kernel::{
         aref::ARef,
         Arc, //
     },
+    time::arch_timer_get_rate,
     transmute::{
         AsBytes,
         FromBytes, //
@@ -266,7 +267,7 @@ impl TyrDrmFileData {
                     Ok(0)
                 }
                 uapi::drm_panthor_dev_query_type_DRM_PANTHOR_DEV_QUERY_TIMESTAMP_INFO => {
-                    let timestamp_frequency = 0u64;
+                    let timestamp_frequency = arch_timer_get_rate().map_or(0, u64::from);
 
                     let _awake = match ddev.pm_context() {
                         Some(ctx) => Some(ctx.get(PMProfile::new().auto())?),
